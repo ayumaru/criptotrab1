ae4541a5efde0edd2b3b690106c7e92689d2a37f
Ayumaru <marllonwes@hotmail.com>
2021-03-17 17:53:38 -0300

    versão final?

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # criptotrab1
 Trabalho prático da materia de criptografia CI1017 - UFPR
+
+## Execução
+
+$> python3 esteganografia.py
+
+## Limitações
+
+Por utilizar a técnica LBS para realizar a esteganografia, o único formato permitido para saída de imagem é ".png". O motivo disso se deve ao fato da dinâmica de compressão de dados que os outros formatos realizam, dessa forma impossibilita que a técnica LBS retire corretamente o texto de outros formatos de imagem.
--- esteganografia.py
+++ esteganografia.py
@@ -1,7 +1,6 @@
 import numpy as np
 import cv2
 import os
-# import types #nao preciso acho
 
 """
 ord > e a funcao que vai pegar o codigo unicode da letra representada por i
@@ -14,14 +13,14 @@ def convert_bin(texto):
         temp = ''
         for i in texto:
             temp += format(ord(i), "08b")
-        # texto = texto.strip('\n')    
-        return temp 
-        # print(''.join( [ format(ord(i), "08b") for i in texto ] ))
-        # return ''.join( [ format(ord(i), "08b") for i in texto ] )   
-    elif type(texto) == bytes or type(texto) == np.ndarray: #tenho impressao que o nparray eh o que vem da memoria
-        return [ format(i, "08b") for i in texto ]
+        return temp     
+ 
+    elif type(texto) == bytes or type(texto) == np.ndarray: #tenho impressao que o nparray eh o que vem da memoria        
+        return np.array( [ format(i, "08b") for i in texto ] )
+
     elif type(texto) == int or type(texto) == np.uint8:
         return format(message, "08b") 
+    
     else:
         print("Mensagem nao suportada")
         raise
@@ -37,30 +36,27 @@ posicao do canal dentro do pixel [r = 0, g = 1 , b = 2]
 """
 def inserir_texto_img(img, txt):
     
-    nb = img.shape[0]*img.shape[1]*3//8
+    nb =((img.shape[0] * img.shape[1])* 3)//8
     print("Tamanho da imagem inserida: ", img.shape[0] ,"x" ,img.shape[1], "\n")
     print("Maximo de caracteres possiveis no texto: ", nb, "\n") #recalcular isso
     print("Seu texto possui: ", len(txt), " caracteres.\n")
 
     txt += "@!@!@" # @ > delimitador do texto
     if len(txt) > nb:
-        print("Quantidade maxima de caracteres excedida")
+        print("Quantidade maxima de caracteres excedida, o programa sera finalizado")
         raise
     
-    # posicao orignal do delimitador
-
     bin_txt = convert_bin(txt)
     #controle da insercao dos dados na imagem
     index = 0
     tam_bin = len(bin_txt) 
-
     for posicao in img:
         for pixel in posicao: 
             
             r,g,b = convert_bin(pixel)
 
             if index < tam_bin:
-                pixel[0] = int( r[:-1] + bin_txt[index], 2 ) # fazer print aqui
+                pixel[0] = int( r[:-1] + bin_txt[index], 2 ) 
                 index += 1
             if index < tam_bin:
                 pixel[1] = int(g[:-1] + bin_txt[index], 2)            
@@ -78,7 +74,7 @@ def retirar_texto(img):
     for posicao in img:
         for pixel in posicao:
             r,g,b = convert_bin(pixel)
-            bin_txt += r[-1] + g[-1] + b[-1] # ver se funciona tudo junto
+            bin_txt += r[-1] + g[-1] + b[-1] 
     
     
     palavra_byte = [ bin_txt[i:i+8] for i in range(0, len(bin_txt), 8) ]
@@ -92,10 +88,27 @@ def retirar_texto(img):
     
     return palavra_txt[:-5]
 
+def carregar_img():
+    nome_img = input()
+    return cv2.imread(nome_img), nome_img
 
+def ler_texto():
+    print("Insira o caminho ate o arquivo texto que deseja esconder: ")
+    nome_txt = input()
+    f = open(nome_txt, "r", encoding='utf8')
+    
+    texto = ''
+    for c in f:
+        texto += c
+
+    final = ""
+    temp = texto.split()
+    final = ' '.join([str(x) for x in temp]) 
+    
+    f.close()
+    return final
 
 def main():
-    # posso colocar o incode e o decode aqui dentro
     print("Esteganografia de texto em imagem. O que deseja fazer? \n")
     print("1 -> Esconder texto em imagem. \n")
     print("2 -> Recuperar texto de uma imagem. \n")
@@ -103,27 +116,24 @@ def main():
     op = int(op)
     if op == 1:
         print("Insira o caminho ate a imagem com o seu formato: \n")
-        nome_img = "teste.jpg"
-        #nome_img = input()
+        imagem, nome_img = carregar_img()
 
-        imagem = cv2.imread(nome_img)
-        if imagem is  None: #arrumar
+        if imagem is None: 
             print("Imagem invalida ou nao encontrada. Finalizando o programa\n")
             return 
 
         print("Insira o nome para a imagem que vai possuir um texto escondido com formato .png: \n")
         temp = True
         while temp:
-            #nome_img_res = input()
-            nome_img_res = "my.png"
+            
+            nome_img_res = input()
             if nome_img_res[-4:] != ".png":
                 print("Formato invalido, insira novamente ou aborte o programa. \n")
             else:
                 temp = False
 
-        print("Insira o texto que deseja esconder na imagem: \n")
-        texto = input() #melhorar isso
-        print(texto)
+        
+        texto = ler_texto()
         if len(texto) == 0:
             print("Nenhum caracter inserido. Finalizando o programa. \n")
 
@@ -134,13 +144,12 @@ def main():
         print("Tamanho da nova imagem:", ( fi.st_size ), "Bytes")
         fi = os.stat(nome_img)
         print("Tamanho da original: ", ( fi.st_size ), " Bytes")
-        ## inserir a verificacao de diferenca de tamanho talvez entre o arquivo original e o resultante?
-
+       
 
     elif op == 2:
         print("Insira o caminho ate a imagem com o seu formato: \n") ## tranformar isso em uma funcao de leitura de imagem
-        nome_img = input()
-        imagem = cv2.imread(nome_img)
+        imagem, nome_img = carregar_img()
+       
         if imagem is None:
             print("Imagem invalida ou nao encontrada. Finalizando o programa\n")
             return
